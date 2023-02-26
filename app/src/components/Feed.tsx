273ae0d3a39b5d8fadd0b8794fa94de2ca406dc3
Zac Ong <61817066+zzacong@users.noreply.github.com>
2023-02-26 14:46:41 +1100

    next: refresh feed

--- app/src/components/Feed.tsx
+++ app/src/components/Feed.tsx
@@ -3,15 +3,24 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import { api } from '~/lib/api';
 import TweetBox from '~/components/TweetBox';
 import Tweet from '~/components/Tweet';
+import { toast } from 'react-hot-toast';
 
 export default function Feed() {
-  const { data: tweets } = api.tweet.getAll.useQuery();
+  const { data: tweets, refetch } = api.tweet.getAll.useQuery();
 
   return (
     <div className="col-span-10 max-h-screen overflow-auto border-x border-x-gray-200 pt-5 md:col-span-9 lg:col-span-6">
       <div className="flex items-center justify-between px-5">
         <h1 className="text-xl font-bold">Home</h1>
-        <ArrowPathIcon className="h-8 w-8 cursor-pointer text-twitter transition duration-500 ease-out hover:rotate-180 active:scale-125" />
+        <button
+          onClick={() => {
+            const id = toast.loading('Refreshing...');
+            void refetch();
+            toast.success('Feed updated!', { id });
+          }}
+        >
+          <ArrowPathIcon className="h-8 w-8 text-twitter transition duration-500 ease-out hover:rotate-180 active:scale-125" />
+        </button>
       </div>
 
       <TweetBox />
