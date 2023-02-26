efa34aef84d142784759376bf64ceaa35778f3c1
Zac Ong <61817066+zzacong@users.noreply.github.com>
2023-02-26 18:33:41 +1100

    next: add image url

--- app/src/components/TweetBox.tsx
+++ app/src/components/TweetBox.tsx
@@ -9,10 +9,35 @@ import {
   MagnifyingGlassCircleIcon,
 } from '@heroicons/react/24/outline';
 import { useSession } from 'next-auth/react';
+import { create } from 'zustand';
+
+const useTweetStore = create<{
+  text: string;
+  imageUrl?: string;
+  setImage: (v: string) => void;
+  setText: (v: string) => void;
+}>(set => ({
+  text: '',
+  imageUrl: undefined,
+  setImage: (v: string) => set({ imageUrl: v }),
+  setText: (v: string) => set({ text: v }),
+}));
 
 export default function TweetBox() {
   const { data: session } = useSession();
   const [input, setInput] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const tweetStore = useTweetStore();
+
+  const onAddImage: React.FormEventHandler<HTMLFormElement> = e => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const imageUrl = form.get('imageUrl');
+    if (typeof imageUrl === 'string' && imageUrl) {
+      tweetStore.setImage(imageUrl);
+    }
+  };
 
   return (
     <div className="flex gap-x-4 px-5 pb-5">
@@ -31,33 +56,58 @@ export default function TweetBox() {
         />
       </div>
 
-      <form className="flex-1">
-        <input
-          type="text"
-          value={input}
-          onChange={e => setInput(e.target.value)}
-          placeholder="What's happening?"
-          className="mb-2 h-14 w-full py-2 text-xl outline-none placeholder:text-xl placeholder:text-gray-500"
-        />
+      <div className="flex-1">
+        <form className="flex-1">
+          <input
+            type="text"
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            placeholder="What's happening?"
+            className="mb-2 h-14 w-full py-2 text-xl outline-none placeholder:text-xl placeholder:text-gray-500"
+          />
 
-        <div className="flex items-center gap-x-4">
-          <div className="flex flex-1 items-center gap-x-2 text-twitter md:gap-x-4">
-            <PhotoIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
-            <MagnifyingGlassCircleIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
-            <FaceSmileIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
-            <CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
-            <MapPinIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+          <div className="flex items-center gap-x-4">
+            <div className="flex flex-1 items-center gap-x-2 text-twitter md:gap-x-4">
+              <button type="button" onClick={() => setIsOpen(p => !p)}>
+                <PhotoIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              </button>
+              <MagnifyingGlassCircleIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <FaceSmileIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <MapPinIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+            </div>
+
+            <button
+              type="submit"
+              disabled={!input || !session}
+              className="rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-50"
+            >
+              Tweet
+            </button>
           </div>
+        </form>
 
-          <button
-            type="submit"
-            disabled={!input}
-            className="rounded-full bg-twitter px-5 py-2 font-bold text-white disabled:opacity-50"
+        {isOpen && (
+          <form
+            onSubmit={onAddImage}
+            className="mt-5 flex gap-x-2 rounded-lg bg-twitter/80 py-2 px-4"
           >
-            Tweet
-          </button>
-        </div>
-      </form>
+            <label htmlFor="imageUrl" className="sr-only">
+              Image URL
+            </label>
+            <input
+              type="text"
+              name="imageUrl"
+              id="imageUrl"
+              placeholder="Enter image URL..."
+              className="flex-1 bg-transparent p-2 text-white placeholder:text-white"
+            />
+            <button type="submit" className="font-bold text-white">
+              Add image
+            </button>
+          </form>
+        )}
+      </div>
     </div>
   );
 }
